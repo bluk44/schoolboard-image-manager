9444b9bc1093ebe71fbb12f680066d4a9497be2f
bluk44 <bluk44@gmail.com>
2013-10-06 22:05:44 +0200

    lol

--- schoolboard-image-manager/src/imageprocessing/plugin/ColorSpacePlugin.java
+++ schoolboard-image-manager/src/imageprocessing/plugin/ColorSpacePlugin.java
@@ -6,6 +6,8 @@ public abstract class ColorSpacePlugin {
 	
 	public abstract BufferedImage[] getHSBchannels(BufferedImage rgbImage);
 	
+	public abstract BufferedImage[] getRGBchannels(BufferedImage rgbImage);
+	
 	public abstract BufferedImage mergeHSBchannels(BufferedImage[] channels);
 	
 }
