6cb4aa07ea87c454dfa29cd228fd5513899a6b26
bluk44 <bluk44@gmail.com>
2013-10-28 10:37:46 +0100

    zmiany w wynikach oznaczania polaczonych regionow

--- schoolboard-image-manager/src/imageprocessing/ConnectedRegionsLabeling.java
+++ schoolboard-image-manager/src/imageprocessing/ConnectedRegionsLabeling.java
@@ -1,13 +1,11 @@
 package imageprocessing;
 
 import ij.process.ImageProcessor;
-import imageprocessing.matrix.MatrixI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 public class ConnectedRegionsLabeling {
 		
@@ -23,9 +21,7 @@ public class ConnectedRegionsLabeling {
 		int nPtr = 0;
 		int currentLabel = 1;
 		DisjointSets labelSet = new DisjointSets(1);
-		
-		Map<Integer, Integer> regions = new HashMap<Integer, Integer>();
-		
+				
 		// first run
 		currentLabel = 1;
 		for(int y=0;y<sizeY;y++){		
@@ -60,25 +56,21 @@ public class ConnectedRegionsLabeling {
 
 		// second run
 
+		int[] regionsSize = new int[labelSet.elements.size()];
+		
 		for(int y=0;y<sizeY;y++){
 			for (int x = 0; x < sizeX; x++) {
-				if(labeled[idx(sizeX, x,y)] == 0) continue;
+				//if(labeled[idx(sizeX, x,y)] == 0) continue;
 				int root = labelSet.find(labeled[idx(sizeX, x,y)]);
 				labeled[idx(sizeX, x,y)] = root;
-				Integer prevVal = regions.get(root);
-				if(prevVal == null){
-					regions.put(root, 1);
-				} else {
-					regions.put(root, prevVal+1);
-				}
 				
+				++regionsSize[root];
 			}
 		}
 		
-		int nreg = regions.size();
-		MatrixI labeledImage = new MatrixI(labeled, sizeX, sizeY);
-
-		return new Results(nreg, regions, labeledImage);
+		
+		
+		return new Results(ip, labeled, regionsSize);
 	}
 		
 	protected static int idx(int sizeX, int x, int y){
@@ -159,28 +151,128 @@ public class ConnectedRegionsLabeling {
 		
 	}
 	
-	public static class Results{
-		private int nLabels;
-		private Map<Integer, Integer> regions;
-		private MatrixI labeledImage;
+	public static class Results {
+		private int nRegions;
+		private ImageProcessor image;
+		private int[] labeledImage;
 		
-		public Results(int nLabels, Map<Integer, Integer> labelSize, MatrixI labeledImage){
-			this.nLabels = nLabels;
-			this.regions = labelSize;
+		private List<Region> regionsList;
+		//private Map<Integer, Region> regionsMap;
+		
+		public Results(ImageProcessor image, int[] labeledImage, int[] regionsSize){
+			this.image = image;
 			this.labeledImage = labeledImage;
+			
+			Map<Integer, Integer> regId_regIdx = new HashMap<Integer, Integer>();
+			int regionsCount = 0;
+			
+			// inicjalizacja mapy ID regionu -> IDX na liscie wszystkich regionow	
+			for(int i = 0; i < regionsSize.length; i++){
+				if(regionsSize[i] == 0) continue;
+				regId_regIdx.put(i, regionsCount++);
+			}
+			
+			// inicjalizacja listy regionow
+			regionsList = new ArrayList<Region>(regionsCount -1);
+			for(int i = 0; i < regionsSize.length; i++){
+				if(regionsSize[i] == 0) continue;
+				regionsList.add(new Region(image, i, regionsSize[i]));
+			}
+			
+			// dodanie mapowania posRegion -> posImage dla kazdego piksela
+			int[] pixelIdxRegion = new int[regionsSize.length];
+			
+			int imgW = image.getWidth(), imgH = image.getHeight();
+			for(int i = 0; i < imgH; i++){
+				for(int j = 0; j < imgW; j++){
+					int regId = labeledImage[i*imgW + j];
+					int posRegion = pixelIdxRegion[regId]++;
+					regionsList.get(regId_regIdx.get(regId)).setPixelMapping(posRegion, i, j);
+				}
+			}
+						
 		}
 		
-		public int getLabelCount(){
-			return nLabels;
+		public Region getRegion(int idx){
+			return regionsList.get(idx);
+		}
+	}
+	
+	public static class Region{
+		ImageProcessor image;
+		Integer id;
+		int size;
+		int minX, maxX, minY, maxY;
+		
+		int[][] imageMapping; // [][0] -> y, [][1] -> x
+		
+		public Region(ImageProcessor image, int id, int size){
+			this.image = image;
+			this.id = id;
+			this.size = size;
+			
+			imageMapping = new int[size][2];
+			minX = image.getWidth();
+			minY = image.getHeight();
 		}
 		
-		public Map<Integer, Integer> getRegions(){
-			return regions;
+		public int[] getPixels(){
+			int[] pixels = new int[size];
+			
+			for(int i = 0; i < imageMapping.length; i++){
+				pixels[i] = (int) image.getPixelValue(imageMapping[i][1], imageMapping[i][0]);
+			}
+
+			return pixels;
 		}
 		
-		public MatrixI getLabeledImage(){
-			return labeledImage;
+		public void setPixel(int idx, int val){
+			image.putPixelValue(imageMapping[idx][1], imageMapping[idx][0], val);
 		}
 		
+		public void setPixels(int[] pixels){
+			if(pixels.length != imageMapping.length) return;
+			
+			for(int i = 0; i < pixels.length; i++){
+				setPixel(i, pixels[i]);
+			}
+		}
+		
+		public void setPixels(int val){
+			for(int i = 0; i < imageMapping.length; i++){
+				setPixel(i, val);
+			}
+		}
+		public void setPixelMapping(int posRegion, int y, int x){
+			imageMapping[posRegion][0] = y;
+			imageMapping[posRegion][1] = x;
+			//System.out.println(posRegion);
+
+			if(y < minY ) minY = y;
+			if(y > maxY ) maxY = y;
+			if(x < minX ) minX = x;
+			if(x > maxX ) maxX = x;			
+			
+		}
+		
+		public int[] getPixelMapping(int posRegion){
+			return imageMapping[posRegion];
+		}
+		
+		public void printRegion(){
+		//	System.out.println(minX+ " "+minY);
+			
+			for(int i = minY; i <= maxY; i++){
+				for(int j = minX; j <= maxX; j++){
+					System.out.print(" "+(int)(image.getPixelValue(j, i)/ 128));
+				}
+				System.out.println();
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return "region: "+id+" size: "+size;
+		}
 	}
 }
--- schoolboard-image-manager/test/imageprocessing/ConnectedRegionsLabelingTest.java
+++ schoolboard-image-manager/test/imageprocessing/ConnectedRegionsLabelingTest.java
@@ -2,12 +2,9 @@ package imageprocessing;
 
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
+import imageprocessing.ConnectedRegionsLabeling.Region;
 import imageprocessing.ConnectedRegionsLabeling.Results;
 import imageprocessing.plugin.ij.AWTImageWrapper;
-
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 public class ConnectedRegionsLabelingTest {
 
@@ -20,17 +17,25 @@ public class ConnectedRegionsLabelingTest {
 		long start = System.currentTimeMillis();
 		Results r = ConnectedRegionsLabeling.run(ip, 0);
 		long stop = System.currentTimeMillis();
-
-		System.out.println("process lasted "+ (stop - start) / 1000.d + " seconds");
-		System.out.println("labels count "+ r.getLabelCount());
-		r.getLabeledImage().print(System.out);
+		System.out.println();
 		
-		Map<Integer, Integer> regions = r.getRegions();
-		Set<Integer> keys = regions.keySet();
-		for (Iterator iterator = keys.iterator(); iterator.hasNext();) {
-			Integer key = (Integer) iterator.next();
-			System.out.println(" "+key+" "+regions.get(key));
-		}
+		Region reg = r.getRegion(2);
+		System.out.println(reg);
+		//reg.setPixels(128);
+		//System.out.println(reg.minY);
+		//System.out.println(reg.minX);
+		//ipl.show();
+		reg.printRegion();
+		
+//		System.out.println("process lasted "+ (stop - start) / 1000.d + " seconds");
+//		System.out.println("labels count "+ r.getLabelCount());
+//		
+//		Map<Integer, Integer> regions = r.getRegions();
+//		Set<Integer> keys = regions.keySet();
+//		for (Iterator iterator = keys.iterator(); iterator.hasNext();) {
+//			Integer key = (Integer) iterator.next();
+//			System.out.println(" "+key+" "+regions.get(key));
+//		}
 	}
 	
 }
