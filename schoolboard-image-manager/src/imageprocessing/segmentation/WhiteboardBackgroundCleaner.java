8e3771306e9210c7b9354e6dd90fe18fcf687900
bluk44 <bluk44@gmail.com>
2013-10-30 22:57:18 +0100

    nowa metoda oczyszczania tła

--- schoolboard-image-manager/src/imageprocessing/segmentation/WhiteboardBackgroundCleaner.java
+++ schoolboard-image-manager/src/imageprocessing/segmentation/WhiteboardBackgroundCleaner.java
@@ -1,25 +1,64 @@
 package imageprocessing.segmentation;
 
+import java.util.Iterator;
+
 import ij.ImagePlus;
 import imageprocessing.CSConverter;
+import imageprocessing.ConnectedRegionsLabeling;
+import imageprocessing.ContrastEnhance;
+import imageprocessing.EdgeDetection;
+import imageprocessing.Morphology;
 import imageprocessing.CSConverter.Conversion;
 import imageprocessing.CSConverter.UnsupportedConversionException;
+import imageprocessing.ConnectedRegionsLabeling.Region;
+import imageprocessing.ConnectedRegionsLabeling.Results;
+import imageprocessing.Morphology.OpType;
+import imageprocessing.Morphology.StructElType;
 import imageprocessing.Thresholding;
 
 
 public class WhiteboardBackgroundCleaner extends BackgroundCleaner {
-
+	private static double SMOOTHING_SCALE = 2.0;
+	private static boolean SUPRESS = true;
+	private static double LOWER = 1.0;
+	private static double HIGHER = 2.0;
+	
+	private static float DILATION_RADIUS = 3.0f;
+	
 	@Override
 	public void clearBackground(ImagePlus rgbImage) {
-
-		
-		try{
-			CSConverter.run(Conversion.STACK_RGB, rgbImage);
-			Thresholding.maxEntropy(rgbImage);
-			CSConverter.run(Conversion.COLOR_RGB, rgbImage);
-		} catch(UnsupportedConversionException ex){
-			ex.printStackTrace();
-		}
+		// zamiana na 8 bit gray
+				try {
+					CSConverter.run(Conversion.GRAY_8, rgbImage);
+				} catch (UnsupportedConversionException e) {
+					System.out.println("conversion failed, terminating... ");
+					return;
+				}
+				
+				// kopiowanie
+				ImagePlus mask = rgbImage.duplicate();
+				
+				// utworzenie maski
+				EdgeDetection.canny(mask, SMOOTHING_SCALE, SUPRESS, LOWER, HIGHER);
+				//mask.show();
+				//System.out.println(mask.getType());
+				
+				Morphology.run(mask, OpType.DILATE, StructElType.CIRCLE, DILATION_RADIUS);
+						
+				// oznaczanie polaczonych regionow
+				Results r = ConnectedRegionsLabeling.run(mask.getProcessor(), 0);
+				
+				// wyrownywanie histogramow
+				for (Iterator it = r.getAllRegions().iterator(); it.hasNext();) {
+					Region reg = (Region) it.next();
+					if(reg.getId() == 0) continue;
+					reg.setImage(rgbImage.getProcessor());
+					int[] pixels = reg.getPixels();
+					ContrastEnhance.equalizeHistogram(pixels, false);
+					Thresholding.IJisodata(pixels);
+					
+					reg.setPixels(pixels);
+				}
 	}
 	
 }
