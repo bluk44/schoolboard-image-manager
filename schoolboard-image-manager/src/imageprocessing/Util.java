6c220b58cb05c401f5fd1e55237c3ad4c1c039bc
bluk44 <bluk44@gmail.com>
2013-11-11 21:53:19 +0100

    dodanie tworzenia bloba z obrazka i odwrotnie

--- schoolboard-image-manager/src/imageprocessing/Util.java
+++ schoolboard-image-manager/src/imageprocessing/Util.java
@@ -1,6 +1,5 @@
 package imageprocessing;
 
-
 import ij.process.ByteProcessor;
 import ij.process.ColorProcessor;
 import ij.process.ImageProcessor;
@@ -11,59 +10,78 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
 /**
  * klasa z podstawowymi operacjami na zdjeciach
+ * 
  * @author Towariszcz_Admin
- *
+ * 
  */
 public abstract class Util {
-	
-	public static BufferedImage copy(BufferedImage src){
-		BufferedImage copy = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
+
+	public static BufferedImage copy(BufferedImage src) {
+		BufferedImage copy = new BufferedImage(src.getWidth(), src.getHeight(),
+				src.getType());
 		src.copyData(copy.getRaster());
-		
+
 		return copy;
 	}
-	
-	public static ImageProcessor copy(ImageProcessor source){
+
+	public static BufferedImage resize(BufferedImage src, double factor) {
+		ImageProcessor resizer = Util.convertToImageProcessor(src);
+		return resizer.resize((int) (src.getWidth() * factor)).getBufferedImage();
 		
-		if(source instanceof ColorProcessor){
-			int[] pixels = Arrays.copyOf((int[]) source.getPixels(), ((int[])source.getPixels()).length);
-			return new ColorProcessor(source.getWidth(), source.getHeight(), pixels);
-			
-		} else if(source instanceof ByteProcessor){
-			byte[] pixels = Arrays.copyOf((byte[]) source.getPixels(), ((byte[])source.getPixels()).length);
-			return new ByteProcessor(source.getWidth(), source.getHeight(), pixels);
-		} else return null;
 	}
-	
-	public static BufferedImage readFromFile(String filename){
+
+	public static ImageProcessor copy(ImageProcessor source) {
+
+		if (source instanceof ColorProcessor) {
+			int[] pixels = Arrays.copyOf((int[]) source.getPixels(),
+					((int[]) source.getPixels()).length);
+			return new ColorProcessor(source.getWidth(), source.getHeight(),
+					pixels);
+
+		} else if (source instanceof ByteProcessor) {
+			byte[] pixels = Arrays.copyOf((byte[]) source.getPixels(),
+					((byte[]) source.getPixels()).length);
+			return new ByteProcessor(source.getWidth(), source.getHeight(),
+					pixels);
+		} else
+			return null;
+	}
+
+	public static BufferedImage readFromFile(String filename) {
 		try {
 			BufferedImage image = ImageIO.read(new File(filename));
 			return image;
 		} catch (IOException e) {
-			System.out.println("unable to open "+filename);
+			System.out.println("unable to open " + filename);
 		}
 		return null;
 	}
-	
-	public static BufferedImage readFromFile(File f){
+
+	public static BufferedImage readFromFile(File f) {
 		try {
 			BufferedImage image = ImageIO.read(f);
 			return image;
 		} catch (IOException e) {
-			System.out.println("unable to open "+f.getName());
+			System.out.println("unable to open " + f.getName());
 		}
 		return null;
 	}
-	
-	public static void writeToFile(String filename, String formatName, BufferedImage img){
+
+	public static void writeToFile(String filename, String formatName,
+			BufferedImage img) {
 		try {
 			File newFile = new File(filename);
 			newFile.createNewFile();
@@ -73,11 +91,24 @@ public abstract class Util {
 			e.printStackTrace();
 		}
 	}
+
+	public static InputStream getBlob(BufferedImage image, String format)
+			throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(image, format, baos);
+		
+		return new ByteArrayInputStream(baos.toByteArray());
+	}
 	
-	public static ImageProcessor convertToImageProcessor(BufferedImage source){
+	public static BufferedImage getImage(Blob blob) throws SQLException, IOException{
+		InputStream imgIs = blob.getBinaryStream();
+		return ImageIO.read(imgIs);
+	}
+	
+	public static ImageProcessor convertToImageProcessor(BufferedImage source) {
 		int numBands = source.getData().getNumBands();
 		ImageProcessor ip = null;
-		switch(numBands){
+		switch (numBands) {
 		case 1:
 			ip = new ByteProcessor(source);
 			break;
@@ -86,37 +117,40 @@ public abstract class Util {
 			break;
 		case 4:
 			ip = new ColorProcessor(source);
-			break;			
+			break;
 		}
 		ip.setRoi(new Rectangle(source.getWidth(), source.getHeight()));
 		return ip;
 	}
-	
-	public static MatrixI grayToMatrixI(BufferedImage grayscaleImg){
-		MatrixI matrix = new MatrixI(grayscaleImg.getWidth(), grayscaleImg.getHeight());
+
+	public static MatrixI grayToMatrixI(BufferedImage grayscaleImg) {
+		MatrixI matrix = new MatrixI(grayscaleImg.getWidth(),
+				grayscaleImg.getHeight());
 		int sizeX = grayscaleImg.getWidth(), sizeY = grayscaleImg.getHeight();
 		Raster r = grayscaleImg.getRaster();
-		for (int i = 0; i < sizeY; i++){
-			for(int j = 0; j < sizeX; j++){
+		for (int i = 0; i < sizeY; i++) {
+			for (int j = 0; j < sizeX; j++) {
 				matrix.setElement(j, i, r.getSample(j, i, 0));
 			}
 		}
 		return matrix;
 	}
-		
-	public static MatrixB grayToMatrixB(BufferedImage grayscaleImg, int threshold){
-		MatrixB matrix = new MatrixB(grayscaleImg.getWidth(), grayscaleImg.getHeight());
+
+	public static MatrixB grayToMatrixB(BufferedImage grayscaleImg,
+			int threshold) {
+		MatrixB matrix = new MatrixB(grayscaleImg.getWidth(),
+				grayscaleImg.getHeight());
 		int sizeX = grayscaleImg.getWidth(), sizeY = grayscaleImg.getHeight();
 		Raster r = grayscaleImg.getRaster();
-		for (int i = 0; i < sizeY; i++){
-			for(int j = 0; j < sizeX; j++){
-				if(r.getSample(j, i, 0) > threshold)
-				matrix.setElement(j, i, true);
+		for (int i = 0; i < sizeY; i++) {
+			for (int j = 0; j < sizeX; j++) {
+				if (r.getSample(j, i, 0) > threshold)
+					matrix.setElement(j, i, true);
 			}
 		}
 		return matrix;
 	}
-	
+
 	public static void inverse(BufferedImage src) {
 		WritableRaster srcR = src.getRaster();
 		int h = src.getHeight(), w = src.getWidth();
@@ -131,8 +165,9 @@ public abstract class Util {
 		}
 
 	}
-	
-	public static float[] conv(int[] samples, int sampW, int sampH, float[][] mask) {
+
+	public static float[] conv(int[] samples, int sampW, int sampH,
+			float[][] mask) {
 
 		int rh = (mask.length - 1) / 2;
 		int rw = (mask[0].length - 1) / 2;
@@ -146,7 +181,7 @@ public abstract class Util {
 				for (int k = -rh; k <= rh; k++) {
 					for (int l = -rw; l <= rw; l++) {
 						sum += samples[sy + k * dsy + sx + l * dsx]
-								* mask[rh+k][rw+l];
+								* mask[rh + k][rw + l];
 					}
 					nsamp[sy + sx] = sum;
 				}
@@ -154,14 +189,15 @@ public abstract class Util {
 		}
 		return nsamp;
 	}
-	
-	public static float[] conv1D(int[] samples, int sampW, int sampH, float[] mask){
+
+	public static float[] conv1D(int[] samples, int sampW, int sampH,
+			float[] mask) {
 		float[] nsamp = new float[sampW * sampH];
-		 
+
 		return nsamp;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		BufferedImage image = Util.readFromFile("");
 	}
 }
