bba2a629145abe82330b5b618c7d3134b635f4fd
bluk44 <bluk44@gmail.com>
2013-11-11 21:50:19 +0100

    inny sposob zmiany wielkosci

--- schoolboard-image-manager/src/imagemanager/gui/ThumbnailComponent.java
+++ schoolboard-image-manager/src/imagemanager/gui/ThumbnailComponent.java
@@ -13,64 +13,65 @@ import javax.swing.JButton;
 
 public class ThumbnailComponent extends JButton {
 
-	private Dimension imageArea;
-	private int imageAreaX, imageAreaY;
+	// ustalane przez rodzica
+	private Dimension thumbImageArea;
+	
+	// położenie obszaru miniaturki (zależne od rzeczywisttch wymiarow,
+	// imageArea i marginesu)
+	private int imageAreaX = 0, imageAreaY = 0;
+
+	// gorny margines
 	private final int margin = 10;
 
-	private ImageProcessor originalImage;
-	private Image thumbnailImage;
-	
+	private BufferedImage originalImage;
+	private BufferedImage thumbnailImage;
+
 	private int imageX, imageY;
+
+	public ThumbnailComponent(int thumbSize) {
+		originalImage = new BufferedImage(250,
+				168, BufferedImage.TYPE_3BYTE_BGR);
+		setThumbImageArea(thumbSize);
+	}
+
+	public ThumbnailComponent(BufferedImage image, int thumbSize) {
+		originalImage = image;
+		setThumbImageArea(thumbSize);
+	}
+
+	public void setThumbImageArea(int imageAreaSize) {
+		thumbImageArea = new Dimension(imageAreaSize, imageAreaSize);
+		setMinimumSize(new Dimension(thumbImageArea.getSize().width + 2
+				* margin, thumbImageArea.getSize().height + 2 * margin));
+		resizeThumbImage();
+	}
 	
-	private static int DEF_COMP_WIDTH = 100, DEF_COMP_HEIGHT = 100;
+	private void resizeThumbImage(){
+		int a = (originalImage.getWidth() > originalImage.getHeight()) ? originalImage.getWidth() : originalImage.getHeight();
+		double ratio = thumbImageArea.getWidth() / a;
+		thumbnailImage = Util.resize(originalImage, ratio);
+
+	}
 	
-	public ThumbnailComponent(){
-		originalImage  = Util.convertToImageProcessor(new BufferedImage(250, 168, BufferedImage.TYPE_3BYTE_BGR));
-		rescale(0.25);
-	}
-	
-	public ThumbnailComponent(BufferedImage image) {
-		originalImage = Util.convertToImageProcessor(image);
-		rescale(0.7);
-	}
-
-	public void rescale(double factor) {
-		createThumbnail(factor);
-		fitThumbArea();
-		changeSize();
-	}
-
-	protected void createThumbnail(double factor) {
-		
-		thumbnailImage = originalImage.resize((int) (originalImage.getWidth()*factor)).getBufferedImage();
-	}
-	protected void fitThumbArea() {
-		int a = (thumbnailImage.getHeight(null) < thumbnailImage.getWidth(null)) ? thumbnailImage
-				.getWidth(null) : thumbnailImage.getHeight(null);
-		imageArea = new Dimension(a, a);
-		imageX = (imageArea.width - thumbnailImage.getWidth(null)) / 2;
-		imageY = (imageArea.height - thumbnailImage.getHeight(null)) / 2;
-	}
-
-	protected void changeSize() {
-	//	System.out.println(getPreferredSize());
-		setPreferredSize(new Dimension(imageArea.width + margin,
-				imageArea.height + margin));
-	}
-
 	@Override
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.BLUE);
-		g.drawRect(imageAreaX, imageAreaY, imageArea.width, imageArea.height);
-		g.drawImage(thumbnailImage, imageAreaX + imageX, imageAreaY + imageY,
-				null);
+		g.drawRect(imageAreaX, imageAreaY, thumbImageArea.width,
+				thumbImageArea.height);
+		g.drawImage(thumbnailImage, imageX, imageY,
+		null);
 	}
 
 	@Override
 	public void setBounds(int x, int y, int width, int height) {
 		super.setBounds(x, y, width, height);
-		imageAreaX = (width - imageArea.width) / 2;
-		imageAreaY = (height - imageArea.height) / 2;
+		imageAreaX = (width - thumbImageArea.width) / 2;
+		imageAreaY = (height - thumbImageArea.height) / 2;
+//		imageX = imageAreaX;
+//		imageY = imageAreaY;
+		imageX = (int) (imageAreaX + ((thumbImageArea.getWidth() - thumbnailImage.getWidth()) / 2.d));
+		imageY = (int) (imageAreaY + ((thumbImageArea.getHeight() - thumbnailImage.getHeight()) / 2.d));
+
 	}
 
 }
