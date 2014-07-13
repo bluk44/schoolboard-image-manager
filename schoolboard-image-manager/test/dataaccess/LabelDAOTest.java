0e94a750958050396e26609b03eb47903299c79b
Lucas Budkovsky <bluk44@gmail.com>
2014-07-13 15:21:09 +0200

    testy generycznych funkcji dao dla Labela

--- schoolboard-image-manager/test/dataaccess/LabelDAOTest.java
+++ schoolboard-image-manager/test/dataaccess/LabelDAOTest.java
@@ -1,17 +1,21 @@
 package dataaccess;
 
-import javax.persistence.EntityManager;
-
 import imagemanager.model.Label;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
 
 public class LabelDAOTest {
 
 	public static void main(String[] args) {
-		saveTest();
-
+		//saveTest();
+		//findByIdTest();
+		 //deleteTest();
+		findAllTest();
 	}
-	
-	public static void saveTest(){
+
+	public static void saveTest() {
 		Label lab = new Label("test label");
 		LabelDAO dao = new LabelDAOImpl();
 		EntityManager em = EclipseLinkUtil.getEntityManager();
@@ -19,5 +23,38 @@ public class LabelDAOTest {
 		dao.save(lab);
 		em.getTransaction().commit();
 		em.close();
+	}
+
+	public static void findByIdTest() {
+		LabelDAO dao = new LabelDAOImpl();
+		EntityManager em = EclipseLinkUtil.getEntityManager();
+		em.getTransaction().begin();
+		Label lab = dao.findByID(Label.class, 101l);
+		em.getTransaction().commit();
+		em.close();
+		System.out.println(lab);
+	}
+	
+	public static void findAllTest(){
+		LabelDAO dao = new LabelDAOImpl();
+		EntityManager em = EclipseLinkUtil.getEntityManager();
+		em.getTransaction().begin();
+		List<Label> labs = dao.findAll(Label.class);
+		em.getTransaction().commit();
+		em.close();
+		for (Label lab : labs) {
+			System.out.println(lab);
+
+		}
+	}
+	
+	public static void deleteTest() {
+		LabelDAO dao = new LabelDAOImpl();
+		EntityManager em = EclipseLinkUtil.getEntityManager();
+		em.getTransaction().begin();
+		Label lab = dao.findByID(Label.class, 101l);
+		dao.delete(lab);
+		em.getTransaction().commit();
+
 	}
 }
