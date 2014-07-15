c25fd4790cd5cb7a38a4e18f7a8d0d35f0aeafbd
Lucas Budkovsky <bluk44@gmail.com>
2014-07-15 18:34:53 +0200

    dawny EclipseLinkUtil

---
+++ schoolboard-image-manager/src/dataaccess/JPAUtil.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private static final String PERSISTENCE_UNIT_NAME = "imgmanager";
+	private static EntityManagerFactory factory;
+	private static EntityManager entityManager;
+	
+	public static EntityManager getEntityManager(){
+		if(factory == null){
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		if(entityManager == null || !entityManager.isOpen()){
+			entityManager = factory.createEntityManager();
+		}
+		return entityManager;
+	}
+	
+	public static EntityManagerFactory getFactory(){
+		return factory;
+	}
+}
